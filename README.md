a750bbdb03a822b7d9d841cdb090177b77680979
muhammad.arifin <arifin.muhammad2610@gmail.com>
2021-02-06 23:58:18 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Responsive Article Viewer
+
+This is the first static web page I made without following any online/offline video tutorials. Everything purely came up from my mind with the help of Mozilla MDN Docs and Javascript code from Brad Traversy (Netflix Clone Tutorial)[https://www.youtube.com/watch?v=P7t13SGytRk&t=81s] which I did several weeks ago. 
+
+In the beginning of the project, I was really just curious about the `onclick` event handling in Javascript. Then I tried the (example from MDN site)[https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onclick]. Then everything just started coming together. All the HTML & CSS I have learned in the past few weeks from the (Internetting Is Hard)[https://www.internetingishard.com/] materials are starting to become intuitive for me. 
+
+Anyway, I haven't really learned Javascript formally yet, but I have a strong programming background from C, C++, and Python programming. So with the docs and the help of the code from Brad's Netflix clone project, I managed to build a very simple site which is responsive and also can handle a simple click event and DOM manipulation. 
+
+This is just the beginning, trust me!
+
+Thanks for stopping by :). 
---
+++ js/script.js
@@ -0,0 +1,70 @@
+// Script for changing header text
+let count = 0;
+document.getElementById('demo').onclick = function changeContent() {
+	if(count == 0) {
+		document.getElementById('demo').textContent = "I'm red now! Click me again!";
+	   	document.getElementById('demo').style = "Color: red";
+	   	count = 1;
+	} else if(count == 1){
+		document.getElementById('demo').textContent = "I'm blue now! Click me again!";
+	   	document.getElementById('demo').style = "Color: blue";
+	   	count = 2;
+	} else {
+		document.getElementById('demo').textContent = "Click Me to reveal a secret!";
+	   	document.getElementById('demo').style = "Color: black";
+	   	count = 0;
+	}
+
+}
+
+/* script for moving from one article to another */
+
+// Variables for manipulating DOM
+const buttonItem = document.querySelectorAll('.btn');
+const contentArticles = document.querySelectorAll('.article');
+
+// select article 
+function selectArticle(e) {
+	// remove show from each article
+	removeShow();
+
+	// remove button color
+	removeButtonColor();
+
+	// check which button is being clicked
+	// console.log(this.id);
+
+	// grab article id from the DOM using btn id
+	var articleItem;
+	if(this.id === 'btn-1') {
+		articleItem = document.getElementById('article-1');
+	} else if (this.id === 'btn-2') {
+		articleItem = document.getElementById('article-2');
+	} else {
+		articleItem = document.getElementById('article-3');
+	}
+
+	// console.log(articleItem);
+
+	// add show class to the chosen article 
+	articleItem.classList.add('show');
+
+	// add background color to the selected button
+	const selectedButton = document.getElementById(this.id);
+	// console.log(selectedButton);
+	selectedButton.classList.add('btn-selected');
+
+}
+
+// remove background color from each button
+function removeButtonColor() {
+	buttonItem.forEach(item => item.classList.remove('btn-selected'));
+}
+
+// remove show class from each article
+function removeShow() {
+	contentArticles.forEach(item => item.classList.remove('show'));
+}
+
+// listen for button click
+buttonItem.forEach(item => item.addEventListener('click', selectArticle));
